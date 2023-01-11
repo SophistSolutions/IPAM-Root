1fb62ad90917dda08250f195f4a96e28b5d5362a
Sterling Wight <sterling@parthenon.sophists.com>
2023-01-11 12:59:46 -0600

    add testharness mechanisms so should run when not in debug mode

--- Tests/Geolocation/Sources/Main.cpp
+++ Tests/Geolocation/Sources/Main.cpp
@@ -15,6 +15,129 @@ using Characters::String;
 
 using namespace IPAM::LibIPAM::Common;
 
+
+/// <summary>
+#include <cstdlib>
+#include <iostream>
+
+#include "Stroika/Foundation/Debug/Assertions.h"
+#include "Stroika/Foundation/Debug/Debugger.h"
+#include "Stroika/Foundation/Debug/Fatal.h"
+#include "Stroika/Foundation/Execution/Exceptions.h"
+#include "Stroika/Foundation/Execution/SignalHandlers.h"
+
+using namespace Stroika;
+using namespace Stroika::Foundation;
+
+namespace {
+    void ASSERT_HANDLER_(const char* assertCategory, const char* assertionText, const char* fileName, int lineNum, const char* functionName) noexcept
+    {
+        if (assertCategory == nullptr) {
+            assertCategory = "Unknown assertion";
+        }
+        if (assertionText == nullptr) {
+            assertionText = "";
+        }
+        if (functionName == nullptr) {
+            functionName = "";
+        }
+        cerr << "FAILED: " << assertCategory << "; " << assertionText << ";" << functionName << ";" << fileName << ": " << lineNum << endl;
+        DbgTrace("FAILED: %s; %s; %s; %s; %d", assertCategory, assertionText, functionName, fileName, lineNum);
+
+        Debug::DropIntoDebuggerIfPresent();
+
+        _Exit(EXIT_FAILURE); // skip
+    }
+    void FatalErrorHandler_(const Characters::SDKChar* msg) noexcept
+    {
+#if qTargetPlatformSDKUseswchar_t
+        cerr << "FAILED: " << Characters::WideStringToNarrowSDKString(msg) << endl;
+#else
+        cerr << "FAILED: " << msg << endl;
+#endif
+        Debug::DropIntoDebuggerIfPresent();
+        _Exit(EXIT_FAILURE); // skip
+    }
+    void FatalSignalHandler_(Execution::SignalID signal) noexcept
+    {
+        cerr << "FAILED: SIGNAL= " << Execution::SignalToName(signal).AsNarrowSDKString() << endl;
+        DbgTrace(L"FAILED: SIGNAL= %s", Execution::SignalToName(signal).c_str());
+        Debug::DropIntoDebuggerIfPresent();
+        _Exit(EXIT_FAILURE); // skip
+    }
+}
+
+namespace Stroika::TestHarness {
+
+    void Setup();
+
+    // print succeeded if it completes, and failed if exception caught
+    int PrintPassOrFail(void (*regressionTest) ());
+
+    // LIKE calling Assert but it will ALSO trigger a failure in NODEBUG builds
+    void Test_(bool failIfFalse, bool isFailureElseWarning, const char* regressionTestText, const char* fileName, int lineNum);
+
+#define VerifyTestResult(c) Stroika::TestHarness::Test_ (!!(c), true, #c, __FILE__, __LINE__)
+#define VerifyTestResultWarning(c) Stroika::TestHarness::Test_ (!!(c), false, #c, __FILE__, __LINE__)
+
+    void WarnTestIssue(const char* issue);
+    void WarnTestIssue(const wchar_t* issue);
+}
+void TestHarness::Setup()
+{
+#if qDebug
+    Stroika::Foundation::Debug::SetAssertionHandler(ASSERT_HANDLER_);
+#endif
+    Debug::RegisterDefaultFatalErrorHandlers(FatalErrorHandler_);
+    using namespace Execution;
+    SignalHandlerRegistry::Get().SetStandardCrashHandlerSignals(SignalHandler{ FatalSignalHandler_, SignalHandler::Type::eDirect });
+}
+int TestHarness::PrintPassOrFail(void (*regressionTest) ())
+{
+    try {
+        (*regressionTest) ();
+        cout << "Succeeded" << endl;
+        DbgTrace(L"Succeeded");
+    }
+    catch (...) {
+        auto exc = current_exception();
+        cerr << "FAILED: REGRESSION TEST DUE TO EXCEPTION: '" << Characters::ToString(exc).AsNarrowSDKString() << "'" << endl;
+        cout << "Failed" << endl;
+        DbgTrace(L"FAILED: REGRESSION TEST (Exception): '%s", Characters::ToString(exc).c_str());
+        Debug::DropIntoDebuggerIfPresent();
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
+
+void TestHarness::Test_(bool failIfFalse, bool isFailureElseWarning, const char* regressionTestText, const char* fileName, int lineNum)
+{
+    if (not failIfFalse) {
+        if (isFailureElseWarning) {
+            ASSERT_HANDLER_("RegressionTestFailure", regressionTestText, fileName, lineNum, "");
+        }
+        else {
+            cerr << "WARNING: REGRESSION TEST ISSUE: " << regressionTestText << ";" << fileName << ": " << lineNum << endl;
+            DbgTrace("WARNING: REGRESSION TEST ISSUE: ; %s; %s; %d", regressionTestText, fileName, lineNum);
+            // OK to continue
+        }
+    }
+}
+void TestHarness::WarnTestIssue(const char* issue)
+{
+    cerr << "WARNING: REGRESSION TEST ISSUE: '" << issue << "'" << endl;
+    DbgTrace("WARNING: REGRESSION TEST ISSUE: '%s", issue);
+}
+
+void TestHarness::WarnTestIssue(const wchar_t* issue)
+{
+    using namespace Characters;
+    string r;
+    WideStringToNarrow(issue, issue + wcslen(issue), GetDefaultSDKCodePage(), &r);
+    WarnTestIssue(r.c_str());
+}
+/// </summary>
+
 namespace {
     void Latitude_Tests()
     {
@@ -108,13 +231,19 @@ namespace {
     }
 }
 
+namespace {
+    void RegressionTests()
+    {
+        Latitude_Tests();
+        Longitude_Tests();
+        TestOne();
+    }
+}
+
 int main ([[maybe_unused]] int argc, [[maybe_unused]] const char* argv[])
 {
     Debug::TraceContextBumper ctx{Stroika_Foundation_Debug_OptionalizeTraceArgs (L"main", L"argv=%s", Characters::ToString (vector<const char*>{argv, argv + argc}).c_str ())};
 
-    Latitude_Tests();
-    Longitude_Tests();
-    TestOne();
-
-    return EXIT_SUCCESS;
+    Stroika::TestHarness::Setup();
+    return Stroika::TestHarness::PrintPassOrFail(RegressionTests);
 }
